367616054dafc5ebf1ab176e0bc3aedd21eacbd5
Liang.CH <kiic.leung@gmail.com>
2014-04-06 19:08:20 +0800

    Update spiders_pikachu.py

--- spiders_pikachu.py
+++ spiders_pikachu.py
@@ -14,10 +14,10 @@ def spiders():
         image_reg_string = r'src="(.*?\.jpg)" '   
         img_reg = re.compile(image_reg_string)
         
-        image_list = re.findall(img_reg,html)
+        image_list = re.findall(img_reg,html) # find all images that match RE 
                 
         for image_url in image_list:
-            urllib.urlretrieve(image_url,'picture_%s.jpg' %image_number)
+            urllib.urlretrieve(image_url,'picture_%s.jpg' %image_number) # download the teledata
             print "Download %s images" %image_number
             image_number += 1
             
